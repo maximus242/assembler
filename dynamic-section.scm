3e53d12a34af7869d117f9ceafd6e4196e93dc5f
Philip Solobay <maximus242@gmail.com>
2024-08-08 16:58:36 -0500

    Updating Headers

--- dynamic-section.scm
+++ dynamic-section.scm
@@ -3,56 +3,21 @@
   #:export (create-dynamic-section))
 
 (define (create-dynamic-section dynstr-offset dynsym-offset strtab-size dynsym-size rela-offset rela-size)
-  ;; Create a bytevector for the dynamic section (9 entries * 16 bytes each)
-  (let ((dynamic-section (make-bytevector 144 0)))
-
-    ;; DT_STRTAB: Points to the dynamic string table
-    ;; Tag value: 5
-    (bytevector-u64-set! dynamic-section 0 5 (endianness little))
-    ;; Address of .dynstr section
-    (bytevector-u64-set! dynamic-section 8 dynstr-offset (endianness little))
-    
-    ;; DT_SYMTAB: Points to the dynamic symbol table
-    ;; Tag value: 6
-    (bytevector-u64-set! dynamic-section 16 6 (endianness little))
-    ;; Address of .dynsym section
-    (bytevector-u64-set! dynamic-section 24 dynsym-offset (endianness little))
-    
-    ;; DT_STRSZ: Size of the dynamic string table
-    ;; Tag value: 10
-    (bytevector-u64-set! dynamic-section 32 10 (endianness little))
-    ;; Size of .dynstr section
-    (bytevector-u64-set! dynamic-section 40 strtab-size (endianness little))
-    
-    ;; DT_SYMENT: Size of a symbol table entry
-    ;; Tag value: 11
-    (bytevector-u64-set! dynamic-section 48 11 (endianness little))
-    ;; Size of Elf64_Sym structure (24 bytes)
-    (bytevector-u64-set! dynamic-section 56 24 (endianness little))
-    
-    ;; DT_RELA: Address of relocation table
-    ;; Tag value: 7
-    (bytevector-u64-set! dynamic-section 64 7 (endianness little))
-    ;; Address of .rela.dyn section
-    (bytevector-u64-set! dynamic-section 72 rela-offset (endianness little))
-    
-    ;; DT_RELASZ: Total size of relocation table
-    ;; Tag value: 8
-    (bytevector-u64-set! dynamic-section 80 8 (endianness little))
-    ;; Size of .rela.dyn section
-    (bytevector-u64-set! dynamic-section 88 rela-size (endianness little))
-    
-    ;; DT_RELAENT: Size of a relocation table entry
-    ;; Tag value: 9
-    (bytevector-u64-set! dynamic-section 96 9 (endianness little))
-    ;; Size of Elf64_Rela structure (24 bytes)
-    (bytevector-u64-set! dynamic-section 104 24 (endianness little))
-    
-    ;; DT_NULL: Marks the end of the dynamic section
-    ;; Tag value: 0
-    (bytevector-u64-set! dynamic-section 112 0 (endianness little))
-    ;; Value (always 0 for DT_NULL)
-    (bytevector-u64-set! dynamic-section 120 0 (endianness little))
-    
-    ;; Return the completed dynamic section
-    dynamic-section))
+  (let ((section (make-bytevector (* 9 16) 0)))  ; 9 entries including DT_NULL
+    (bytevector-u64-set! section 0 5 (endianness little))  ; DT_STRTAB
+    (bytevector-u64-set! section 8 dynstr-offset (endianness little))
+    (bytevector-u64-set! section 16 6 (endianness little))  ; DT_SYMTAB
+    (bytevector-u64-set! section 24 dynsym-offset (endianness little))
+    (bytevector-u64-set! section 32 10 (endianness little))  ; DT_STRSZ
+    (bytevector-u64-set! section 40 strtab-size (endianness little))
+    (bytevector-u64-set! section 48 11 (endianness little))  ; DT_SYMENT
+    (bytevector-u64-set! section 56 24 (endianness little))
+    (bytevector-u64-set! section 64 7 (endianness little))  ; DT_RELA
+    (bytevector-u64-set! section 72 rela-offset (endianness little))
+    (bytevector-u64-set! section 80 8 (endianness little))  ; DT_RELASZ
+    (bytevector-u64-set! section 88 rela-size (endianness little))
+    (bytevector-u64-set! section 96 9 (endianness little))  ; DT_RELAENT
+    (bytevector-u64-set! section 104 24 (endianness little))
+    (bytevector-u64-set! section 112 0 (endianness little))  ; DT_NULL
+    (bytevector-u64-set! section 120 0 (endianness little))
+    section))
--- symbol-table.scm
+++ symbol-table.scm
@@ -2,10 +2,12 @@
   #:use-module (ice-9 hash-table)
   #:use-module (rnrs bytevectors)
   #:use-module (rnrs io ports)
+  #:use-module (srfi srfi-1)
   #:export (make-symbol-table
             add-symbol!
             get-symbol
-            create-symbol-table))
+            create-symbol-table
+            create-dynamic-symbol-table))
 
 (define (make-symbol-table)
   (make-hash-table))
@@ -16,16 +18,27 @@
 (define (get-symbol table name)
   (hash-ref table name))
 
-;; Helper functions to create various parts of the ELF file
 (define (create-symbol-table symbol-addresses)
-  (let* ((symbol-count (length symbol-addresses))
-         (table-size (* symbol-count 24))  ; Each symbol entry is 24 bytes
+  (let* ((symbol-count (+ (length symbol-addresses) 1))  ; Add 1 for 'main'
+         (table-size (* symbol-count 24))
          (table (make-bytevector table-size 0))
          (string-table-offset 1))  ; Start at 1 to account for null byte at beginning of string table
+
     (format #t "Creating symbol table with ~a symbols~%" symbol-count)
+
+    ; Add 'main' symbol
+    (bytevector-u32-set! table 0 1 (endianness little))  ; st_name (offset in string table)
+    (bytevector-u8-set! table 4 18)  ; st_info (global function)
+    (bytevector-u8-set! table 5 0)   ; st_other
+    (bytevector-u16-set! table 6 1 (endianness little))  ; st_shndx (1 for .text)
+    (bytevector-u64-set! table 8 #x1000 (endianness little))  ; st_value (entry point)
+    (bytevector-u64-set! table 16 0 (endianness little))  ; st_size (unknown)
+
+    (format #t "Added symbol: main, address: 0x1000, offset in string table: 1~%")
+
     (let loop ((symbols symbol-addresses)
-               (index 0)
-               (str-offset 1))
+               (index 1)  ; Start at 1 because 'main' is index 0
+               (str-offset 6))  ; 'main\0' takes 5 bytes
       (if (null? symbols)
           (begin
             (format #t "Symbol table created. Size: ~a bytes~%" (bytevector-length table))
@@ -34,12 +47,52 @@
                  (name (symbol->string (car symbol)))
                  (address (cdr symbol)))
             (bytevector-u32-set! table (* index 24) str-offset (endianness little))  ; st_name
-            (bytevector-u8-set! table (+ (* index 24) 4) 1)  ; st_info (1 = STT_OBJECT)
+            (bytevector-u8-set! table (+ (* index 24) 4) 17)  ; st_info (17 = STT_OBJECT | STB_GLOBAL)
             (bytevector-u8-set! table (+ (* index 24) 5) 0)  ; st_other
-            (bytevector-u16-set! table (+ (* index 24) 6) 1 (endianness little))  ; st_shndx (1 = .text section)
+            (bytevector-u16-set! table (+ (* index 24) 6) 2 (endianness little))  ; st_shndx (2 = .data section)
             (bytevector-u64-set! table (+ (* index 24) 8) address (endianness little))  ; st_value
-            (bytevector-u64-set! table (+ (* index 24) 16) 0 (endianness little))  ; st_size
+            (bytevector-u64-set! table (+ (* index 24) 16) 32 (endianness little))  ; st_size (assuming 32-byte size for data objects)
             (format #t "Added symbol: ~a, address: 0x~x, offset in string table: ~a~%" 
+                    name address str-offset)
+            (loop (cdr symbols)
+                  (+ index 1)
+                  (+ str-offset (string-length name) 1)))))))
+
+(define (create-dynamic-symbol-table symbol-addresses)
+  (let* ((symbol-count (+ (length symbol-addresses) 1))  ; Add 1 for 'main'
+         (table-size (* symbol-count 24))
+         (table (make-bytevector table-size 0))
+         (string-table-offset 1))
+
+    (format #t "Creating dynamic symbol table with ~a symbols~%" symbol-count)
+
+    ; Add 'main' symbol
+    (bytevector-u32-set! table 0 1 (endianness little))  ; st_name (offset in string table)
+    (bytevector-u8-set! table 4 18)  ; st_info (global function)
+    (bytevector-u8-set! table 5 0)   ; st_other
+    (bytevector-u16-set! table 6 1 (endianness little))  ; st_shndx (1 for .text)
+    (bytevector-u64-set! table 8 #x1000 (endianness little))  ; st_value (entry point)
+    (bytevector-u64-set! table 16 0 (endianness little))  ; st_size (unknown)
+
+    (format #t "Added dynamic symbol: main, address: 0x1000, offset in string table: 1~%")
+
+    (let loop ((symbols symbol-addresses)
+               (index 1)  ; Start at 1 because 'main' is index 0
+               (str-offset 6))  ; 'main\0' takes 5 bytes
+      (if (null? symbols)
+          (begin
+            (format #t "Dynamic symbol table created. Size: ~a bytes~%" (bytevector-length table))
+            table)
+          (let* ((symbol (car symbols))
+                 (name (symbol->string (car symbol)))
+                 (address (cdr symbol)))
+            (bytevector-u32-set! table (* index 24) str-offset (endianness little))  ; st_name
+            (bytevector-u8-set! table (+ (* index 24) 4) 17)  ; st_info (17 = STT_OBJECT | STB_GLOBAL)
+            (bytevector-u8-set! table (+ (* index 24) 5) 0)  ; st_other
+            (bytevector-u16-set! table (+ (* index 24) 6) 2 (endianness little))  ; st_shndx (2 = .data section)
+            (bytevector-u64-set! table (+ (* index 24) 8) address (endianness little))  ; st_value
+            (bytevector-u64-set! table (+ (* index 24) 16) 32 (endianness little))  ; st_size (assuming 32-byte size for data objects)
+            (format #t "Added dynamic symbol: ~a, address: 0x~x, offset in string table: ~a~%" 
                     name address str-offset)
             (loop (cdr symbols)
                   (+ index 1)
