48d580ee8c328a251ca87e956827c597db7a90b4
Philip Solobay <maximus242@gmail.com>
2024-08-10 12:53:17 -0500

    Clean up config

--- config.scm
+++ config.scm
@@ -1,56 +1,59 @@
 (define-module (config)
-  #:export (elf-header-size
-            entry-point
-            code-offset
-            alignment
-            dynamic-entry-size
-            got-entry-size
-            section-header-size
-            program-header-size
-            num-sections
-            shstrtab-index
-            elf-magic
-            elf-class-64
-            elf-data-lsb
-            elf-version-current
-            elf-osabi-linux
-            elf-type-shared
-            elf-machine-x86-64
-            sht-null
-            sht-progbits
-            sht-symtab
-            sht-strtab
-            sht-rela
-            sht-dynamic
-            sht-nobits
-            sht-dynsym
-            shf-write
-            shf-alloc
-            shf-execinstr
-            pt-load
-            pt-dynamic
-            pt-phdr
-            pt-gnu-relro
-            pf-x
-            pf-w
-            pf-r
-            num-program-headers
-            text-addr
-            shstrtab-addr
-            shstrtab-size
-            section-offset))
+  #:export (
+    ;; ELF Header Constants
+    elf-header-size
+    entry-point
+    code-offset
+    alignment
+    elf-magic
+    elf-class-64
+    elf-data-lsb
+    elf-version-current
+    elf-osabi-linux
+    elf-type-shared
+    elf-machine-x86-64
 
+    ;; Section Header Constants
+    section-header-size
+    num-sections
+    shstrtab-index
+    sht-null
+    sht-progbits
+    sht-symtab
+    sht-strtab
+    sht-rela
+    sht-dynamic
+    sht-nobits
+    sht-dynsym
+    shf-write
+    shf-alloc
+    shf-execinstr
+
+    ;; Program Header Constants
+    program-header-size
+    num-program-headers
+    pt-load
+    pt-dynamic
+    pt-phdr
+    pt-gnu-relro
+    pf-x
+    pf-w
+    pf-r
+
+    ;; Other Constants
+    dynamic-entry-size
+    got-entry-size
+    text-addr
+    shstrtab-addr
+    shstrtab-size
+    section-offset
+  ))
+
+;; ELF Header Constants
 (define elf-header-size 64)
 (define entry-point #x1000)
 (define code-offset #x1000)
 (define alignment #x1000)
-(define dynamic-entry-size 16)
-(define got-entry-size 8)
-(define section-header-size 64)
-(define program-header-size 56)
-(define num-sections 14)
-(define shstrtab-index 13)
-
 (define elf-magic #x464c457f)
 (define elf-class-64 2)
 (define elf-data-lsb 1)
@@ -59,7 +62,10 @@
 (define elf-type-shared 3)
 (define elf-machine-x86-64 #x3e)
 
-;; New constants from section-headers
+;; Section Header Constants
+(define section-header-size 64)
+(define num-sections 14)
+(define shstrtab-index 13)
 (define sht-null 0)
 (define sht-progbits 1)
 (define sht-symtab 2)
@@ -68,25 +74,25 @@
 (define sht-dynamic 6)
 (define sht-nobits 8)
 (define sht-dynsym 11)
-
 (define shf-write #x1)
 (define shf-alloc #x2)
 (define shf-execinstr #x4)
 
-;; New constants from program-headers
+;; Program Header Constants
+(define program-header-size 56)
+(define num-program-headers 5)
 (define pt-load 1)
 (define pt-dynamic 2)
 (define pt-phdr 6)
 (define pt-gnu-relro #x6474e552)
-
 (define pf-x #b001)
 (define pf-w #b010)
 (define pf-r #b100)
 
-(define num-program-headers 5)
-
+;; Other Constants
+(define dynamic-entry-size 16)
+(define got-entry-size 8)
 (define text-addr #x1000)
 (define shstrtab-addr #x3f94)
 (define shstrtab-size 108)
-
 (define section-offset #x2000)
