74c525282f11759b8feab6cc02b55d7dbe5185f2
Philip Solobay <maximus242@gmail.com>
2024-08-12 13:29:56 -0500

    Refactor relocation table

--- relocation-table.scm
+++ relocation-table.scm
@@ -2,11 +2,19 @@
   #:use-module (rnrs bytevectors)
   #:export (create-relocation-table))
 
-;; Section creation functions
+;; Constants for relocation entry
+(define RELOC_ENTRY_SIZE 24)
+(define R_OFFSET_OFFSET 0)
+(define R_INFO_OFFSET 8)
+(define R_ADDEND_OFFSET 16)
 
+;; Constants for relocation types
+(define R_X86_64_64 1)
+
+;; Function to create relocation table
 (define (create-relocation-table symbol-addresses)
   (let* ((reloc-count (length symbol-addresses))
-         (table-size (* reloc-count 24))
+         (table-size (* reloc-count RELOC_ENTRY_SIZE))
          (table (make-bytevector table-size 0)))
     (let loop ((symbols symbol-addresses)
                (index 0))
@@ -14,9 +22,18 @@
         table
         (let* ((symbol (car symbols))
                (name (car symbol))
-               (address (cdr symbol)))
-          (bytevector-u64-set! table (* index 24) address (endianness little))  ; r_offset
-          (bytevector-u64-set! table (+ (* index 24) 8)
-                               (logior (ash index 32) 1) (endianness little))  ; r_info (1 = R_X86_64_64)
-          (bytevector-u64-set! table (+ (* index 24) 16) 0 (endianness little))  ; r_addend
+               (address (cdr symbol))
+               (entry-offset (* index RELOC_ENTRY_SIZE)))
+          (bytevector-u64-set! table 
+                               (+ entry-offset R_OFFSET_OFFSET) 
+                               address 
+                               (endianness little))  ; r_offset
+          (bytevector-u64-set! table 
+                               (+ entry-offset R_INFO_OFFSET)
+                               (logior (ash index 32) R_X86_64_64) 
+                               (endianness little))  ; r_info
+          (bytevector-u64-set! table 
+                               (+ entry-offset R_ADDEND_OFFSET) 
+                               0 
+                               (endianness little))  ; r_addend
           (loop (cdr symbols) (+ index 1)))))))
