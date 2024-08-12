d7ecdb1dc481cf28d18643e60df42445e57dcd49
Philip Solobay <maximus242@gmail.com>
2024-08-12 14:44:49 -0500

    refactor

--- linker.scm
+++ linker.scm
@@ -14,6 +14,20 @@
 
 ;; Constants
 (define base-address-difference #x1000) ; 0x401000 - 0x400000
+(define rex-w #x48)
+(define mov-immediate #xC7)
+(define vmovaps-prefix #xC5)
+(define vmovaps-opcode #xFC)
+(define vmovaps-mod-rm #x28)
+(define vmovaps-sib #x05)
+(define call-opcode #xE8)
+(define jmp-opcode #xE9)
+(define register-mask #x07)
+(define immediate-size 4)
+(define mov-immediate-total-size 7)
+(define vmovaps-total-size 8)
+(define call-jmp-total-size 5)
+(define lea-instruction-size 7)
 
 ;; Helper functions
 (define (alist-ref key alist)
@@ -43,80 +57,48 @@
       ((= (bytevector-u8-ref bv i) byte) i)
       (else (loop (+ i 1))))))
 
-(define (display-bytevector bv)
-  (let ((len (bytevector-length bv)))
-    (do ((i 0 (+ i 1)))
-      ((= i len))
-      (format #t "~2,'0x " (bytevector-u8-ref bv i))
-      (when (= (modulo (+ i 1) 16) 0)
-        (newline)))))
-
 ;; Instruction handling functions
 (define (handle-mov-immediate code resolved-code offset symbol-table label-positions reg-to-symbol-map code-base-address)
-  (let* ((reg (logand (bytevector-u8-ref code (+ offset 2)) #x07))
+  (let* ((reg (logand (bytevector-u8-ref code (+ offset 2)) register-mask))
          (imm-offset (+ offset 3)))
-    (if (< (+ imm-offset 4) (bytevector-length code))
+    (if (< (+ imm-offset immediate-size) (bytevector-length code))
       (let ((imm (bytevector-u32-ref code imm-offset (endianness little))))
-        (format #t "  MOV imm32: reg=~a, imm=~x~%" reg imm)
         (when (= imm 0) ; Possible symbolic reference or label
           (let* ((symbol-name (alist-ref reg reg-to-symbol-map))
                  (symbol-address (and symbol-name (or (alist-ref symbol-name symbol-table)
                                                       (hash-ref label-positions symbol-name)))))
-            (format #t "  Resolving symbol/label: ~a -> ~a~%" 
-                    (or symbol-name "#f") 
-                    (if symbol-address 
-                      (format #f "~x" symbol-address)
-                      "#f"))
             (when symbol-address
               (bytevector-u32-set! resolved-code imm-offset 
                                    (if (hash-ref label-positions symbol-name)
                                      (+ symbol-address code-base-address)
                                      (adjust-address symbol-address))
                                    (endianness little)))))
-        (format #t "  After MOV resolution: ~x~%" 
-                (bytevector-u32-ref resolved-code imm-offset (endianness little)))
-        (+ offset 7))
-      (begin
-        (format #t "Warning: Insufficient bytes for MOV immediate at offset ~a~%" offset)
-        (+ offset 1)))))
+        (+ offset mov-immediate-total-size))
+      (+ offset 1))))
 
 (define (handle-vmovaps code resolved-code offset symbol-table code-base-address)
   (let* ((imm-offset (+ offset 4))
          (imm (bytevector-u32-ref code imm-offset (endianness little)))
          (symbol-address (alist-ref 'multiplier symbol-table)))
-    (format #t "  VMOVAPS: original displacement=~x~%" imm)
     (when symbol-address
-      (let* ((instruction-end (+ offset 8))
+      (let* ((instruction-end (+ offset vmovaps-total-size))
              (next-instruction-address (+ instruction-end code-base-address))
              (target-offset (- (adjust-address symbol-address) next-instruction-address)))
-        (format #t "    Resolving multiplier -> ~x~%" symbol-address)
-        (format #t "    Instruction end: ~x~%" instruction-end)
-        (format #t "    Next instruction address: ~x~%" next-instruction-address)
-        (format #t "    Target offset: ~x~%" target-offset)
         (bytevector-u32-set! resolved-code imm-offset target-offset (endianness little))))
-    (format #t "  After VMOVAPS resolution: ~x~%" 
-            (bytevector-u32-ref resolved-code imm-offset (endianness little)))
-    (+ offset 8)))
+    (+ offset vmovaps-total-size)))
 
 (define (handle-call-jmp code resolved-code offset symbol-table label-positions reg-to-symbol-map code-base-address)
   (let* ((imm-offset (+ offset 1))
          (imm (bytevector-s32-ref code imm-offset (endianness little))))
-    (format #t "  CALL/JMP: original offset=~x~%" imm)
     (when (= imm 0) ; Possible label reference
       (let* ((label-name (alist-ref offset reg-to-symbol-map))
              (label-position (and label-name (hash-ref label-positions label-name))))
         (when label-position
-          (let* ((instruction-end (+ offset 5))
+          (let* ((instruction-end (+ offset call-jmp-total-size))
                  (next-instruction-address (+ instruction-end code-base-address))
                  (target-offset (- (+ label-position code-base-address) next-instruction-address)))
-            (format #t "    Resolving label ~a -> ~x~%" label-name label-position)
-            (format #t "    Instruction end: ~x~%" instruction-end)
-            (format #t "    Next instruction address: ~x~%" next-instruction-address)
-            (format #t "    Target offset: ~x~%" target-offset)
             (bytevector-s32-set! resolved-code imm-offset target-offset (endianness little))))))
-    (format #t "  After CALL/JMP resolution: ~x~%" 
-            (bytevector-s32-ref resolved-code imm-offset (endianness little)))
-    (+ offset 5)))
+    (+ offset call-jmp-total-size)))
 
 ;; Main functions
 (define (resolve-references code symbol-table label-positions reg-to-symbol-map)
@@ -124,33 +106,27 @@
          (resolved-code (bytevector-copy code))
          (code-base-address 0)
          (program-base-address 0))
-    (format #t "Starting to resolve references. Code length: ~a~%" code-length)
-    (format #t "Symbol table: ~a~%" symbol-table)
-    (format #t "Label positions: ~a~%" label-positions)
     (let loop ((offset 0))
       (if (< offset code-length)
         (let ((instruction (bytevector-u8-ref code offset)))
-          (format #t "Offset: ~a, Instruction: ~x~%" offset instruction)
           (cond
-            ((and (= instruction #x48)
+            ((and (= instruction rex-w)
                   (< (+ offset 6) code-length)
-                  (= (bytevector-u8-ref code (+ offset 1)) #xC7))
+                  (= (bytevector-u8-ref code (+ offset 1)) mov-immediate))
              (loop (handle-mov-immediate code resolved-code offset symbol-table label-positions reg-to-symbol-map code-base-address)))
-            ((and (= instruction #xC5)
+            ((and (= instruction vmovaps-prefix)
                   (< (+ offset 7) code-length)
-                  (= (bytevector-u8-ref code (+ offset 1)) #xFC)
-                  (= (bytevector-u8-ref code (+ offset 2)) #x28)
-                  (= (bytevector-u8-ref code (+ offset 3)) #x05))
+                  (= (bytevector-u8-ref code (+ offset 1)) vmovaps-opcode)
+                  (= (bytevector-u8-ref code (+ offset 2)) vmovaps-mod-rm)
+                  (= (bytevector-u8-ref code (+ offset 3)) vmovaps-sib))
              (loop (handle-vmovaps code resolved-code offset symbol-table code-base-address)))
-            ((and (or (= instruction #xE8) ; CALL
-                      (= instruction #xE9)) ; JMP
+            ((and (or (= instruction call-opcode) 
+                      (= instruction jmp-opcode))
                   (< (+ offset 4) code-length))
              (loop (handle-call-jmp code resolved-code offset symbol-table label-positions reg-to-symbol-map code-base-address)))
             (else 
               (loop (+ offset 1)))))
-        (begin
-          (format #t "Finished resolving references.~%")
-          resolved-code)))))
+        resolved-code))))
 
 (define (link-code code symbol-addresses label-positions)
   (let ((linked-code (bytevector-copy code))
@@ -160,8 +136,8 @@
         (when (eq? (car inst) 'lea)
           (let* ((label (caddr (cadr inst)))
                  (target-address (cdr (assoc label symbol-addresses)))
-                 (instruction-end (+ code-offset 7))
-                 (next-instruction-address (+ code-offset 7))
+                 (instruction-end (+ code-offset lea-instruction-size))
+                 (next-instruction-address (+ code-offset lea-instruction-size))
                  (displacement (- target-address next-instruction-address)))
             (bytevector-u32-set! linked-code (+ code-offset 3) displacement (native-endianness)))))
       (disassemble code))
