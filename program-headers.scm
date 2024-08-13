261ed73ece4c45b6d23304ff33b2cb6cfb4f8878
Philip Solobay <maximus242@gmail.com>
2024-08-13 12:53:28 -0500

    working fix for error

--- program-headers.scm
+++ program-headers.scm
@@ -87,7 +87,7 @@
                                    text-segment-end text-segment-end
                                    alignment)
               ;; PT_LOAD for .data, .bss, .dynamic, .got (read-write)
-              (make-program-header pt-load (logior pf-r pf-w) 
+              (make-program-header pt-load (logior pf-r pf-w pf-x) 
                                    data-segment-start
                                    data-segment-start data-segment-start
                                    data-segment-file-size data-segment-mem-size
