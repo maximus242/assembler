c3c1ef185d4fb745fb8314006c6e168503b9c579
Philip Solobay <maximus242@gmail.com>
2024-08-16 10:25:58 -0500

    LABEL - Symbol Table Tests Passing

--- symbol-table.scm
+++ symbol-table.scm
@@ -7,7 +7,10 @@
   #:use-module (string-table)
   #:export (make-symbol-table
             add-symbol!
+            add-label-symbol!
             get-symbol
+            symbol-in-table?
+            symbol-is-function?
             create-symbol-table
             create-dynamic-symbol-table
             create-hash-section
@@ -26,71 +29,37 @@
   (shndx symbol-entry-shndx)
   (size symbol-entry-size))
 
-;; Convert a bytevector to a list of its elements for debugging purposes
-(define (bytevector->list bv)
-  (let loop ((i 0) (lst '()))
-    (if (= i (bytevector-length bv))
-        (reverse lst)
-        (loop (+ i 1) (cons (bytevector-u8-ref bv i) lst)))))
-
 (define (make-symbol-table)
   (make-hash-table))
 
 (define (add-symbol! table name address)
-  (hash-set! table name address))
+  (hash-set! table name (cons address #f)))  ; #f indicates it's not a function
+
+(define (add-label-symbol! table name address)
+  (hash-set! table name (cons address #t)))  ; #t indicates it's a function
 
 (define (get-symbol table name)
+  (let ((entry (hash-ref table name #f)))
+    (and entry (car entry))))
+
+(define (symbol-in-table? table name)
   (hash-ref table name #f))
 
-(define* (create-symbol-table symbol-addresses #:optional (options '()))
-  (let ((symbol-entry-size (or (assoc-ref options 'symbol-entry-size) 24))
-        (st-name-offset (or (assoc-ref options 'st-name-offset) 0))
-        (st-info-offset (or (assoc-ref options 'st-info-offset) 4))
-        (st-other-offset (or (assoc-ref options 'st-other-offset) 5))
-        (st-shndx-offset (or (assoc-ref options 'st-shndx-offset) 6))
-        (st-value-offset (or (assoc-ref options 'st-value-offset) 8))
-        (st-size-offset (or (assoc-ref options 'st-size-offset) 16))
-        (stt-object (or (assoc-ref options 'stt-object) 1))
-        (stb-global (or (assoc-ref options 'stb-global) 1))
-        (stv-default (or (assoc-ref options 'stv-default) 0))
-        (shn-data (or (assoc-ref options 'shn-data) 2)))  ; Changed to .data section
-    (let* ((symbol-count (+ (length symbol-addresses) 1))
-           (table-size (* symbol-count symbol-entry-size))
-           (table (make-bytevector table-size 0))
-           (string-table (create-string-table symbol-addresses)))
-      ;; Initialize the first symbol entry (null symbol)
-      (bytevector-u32-set! table st-name-offset 0 (endianness little))
-      (bytevector-u8-set! table st-info-offset 0)
-      (bytevector-u8-set! table st-other-offset 0)
-      (bytevector-u16-set! table st-shndx-offset 0 (endianness little))
-      (bytevector-u64-set! table st-value-offset 0 (endianness little))
-      (bytevector-u64-set! table st-size-offset 0 (endianness little))
-      ;; Process the symbol addresses
-      (let loop ((symbols symbol-addresses)
-                 (index 1)
-                 (str-offset 1))  ; Start at 1 to skip the null byte
-        (if (null? symbols)
-            (cons table string-table)
-            (let* ((symbol (car symbols))
-                   (name (symbol->string (car symbol)))
-                   (address (cdr symbol))
-                   (entry-offset (* index symbol-entry-size))
-                   (st-info (logior (ash stb-global 4) stt-object)))
-              ;; Write the name offset
-              (bytevector-u32-set! table (+ entry-offset st-name-offset) str-offset (endianness little))
-              ;; Write the info (global object) and other fields
-              (bytevector-u8-set! table (+ entry-offset st-info-offset) st-info)
-              (bytevector-u8-set! table (+ entry-offset st-other-offset) stv-default)
-              ;; Set the section index to .data instead of .bss
-              (bytevector-u16-set! table (+ entry-offset st-shndx-offset) shn-data (endianness little))
-              ;; Write the address
-              (bytevector-u64-set! table (+ entry-offset st-value-offset) address (endianness little))
-              (bytevector-u64-set! table (+ entry-offset st-size-offset) 32 (endianness little))
-              (loop (cdr symbols)
-                    (+ index 1)
-                    (+ str-offset (string-length name) 1))))))))
+(define (symbol-is-function? table name)
+  (let ((entry (hash-ref table name #f)))
+    (and entry (cdr entry))))
 
-(define* (create-dynamic-symbol-table symbol-addresses #:optional (options '()))
+(define* (create-symbol-table symbol-addresses label-positions #:optional (options '()))
+  (let ((table (make-symbol-table)))
+    (for-each (lambda (sym)
+                (add-symbol! table (car sym) (cdr sym)))
+              symbol-addresses)
+    (for-each (lambda (label)
+                (add-label-symbol! table (car label) (cdr label)))
+              label-positions)
+    table))
+
+(define* (create-dynamic-symbol-table symbol-table #:optional (options '()))
   (let ((symbol-entry-size (or (assoc-ref options 'symbol-entry-size) 24))
         (st-name-offset (or (assoc-ref options 'st-name-offset) 0))
         (st-info-offset (or (assoc-ref options 'st-info-offset) 4))
@@ -101,9 +70,11 @@
         (null-terminator-size (or (assoc-ref options 'null-terminator-size) 1))
         (initial-string-offset (or (assoc-ref options 'initial-string-offset) 1))
         (stt-object (or (assoc-ref options 'stt-object) 1))
+        (stt-func (or (assoc-ref options 'stt-func) 2))
         (stb-global (or (assoc-ref options 'stb-global) 1))
-        (shn-data (or (assoc-ref options 'shn-data) 2)))  ; Section index for .data
-    (let* ((symbol-count (+ (length symbol-addresses) 1))
+        (shn-data (or (assoc-ref options 'shn-data) 2))
+        (shn-text (or (assoc-ref options 'shn-text) 1)))
+    (let* ((symbol-count (+ (hash-fold (lambda (key value acc) (+ acc 1)) 0 symbol-table) 1))
            (table-size (* symbol-count symbol-entry-size))
            (table (make-bytevector table-size 0))
            (string-table-offset initial-string-offset))
@@ -114,61 +85,33 @@
       (bytevector-u16-set! table st-shndx-offset 0 (endianness little))
       (bytevector-u64-set! table st-value-offset 0 (endianness little))
       (bytevector-u64-set! table st-size-offset 0 (endianness little))
-      ;; Process the symbol addresses
-      (let loop ((symbols symbol-addresses)
+      ;; Process the symbols
+      (let loop ((symbols (hash-map->list cons symbol-table))
                  (index 1)
                  (str-offset initial-string-offset))
         (if (null? symbols)
             table
             (let* ((symbol (car symbols))
                    (name (symbol->string (car symbol)))
-                   (address (cdr symbol))
+                   (value (cdr symbol))
+                   (address (car value))
+                   (is-function (cdr value))
                    (entry-offset (* index symbol-entry-size))
-                   (st-info (logior (ash stb-global 4) stt-object)))
+                   (st-info (logior (ash stb-global 4) (if is-function stt-func stt-object)))
+                   (shndx (if is-function shn-text shn-data)))
               ;; Write the name offset
               (bytevector-u32-set! table (+ entry-offset st-name-offset) str-offset (endianness little))
-              ;; Write the info (global object) and other fields
+              ;; Write the info (global object/function) and other fields
               (bytevector-u8-set! table (+ entry-offset st-info-offset) st-info)
               (bytevector-u8-set! table (+ entry-offset st-other-offset) 0)
-              (bytevector-u16-set! table (+ entry-offset st-shndx-offset) shn-data (endianness little))
+              (bytevector-u16-set! table (+ entry-offset st-shndx-offset) shndx (endianness little))
               ;; Write the address
               (bytevector-u64-set! table (+ entry-offset st-value-offset) address (endianness little))
-              ;; Set the size field to 0 (or actual size if known)
-              (bytevector-u64-set! table (+ entry-offset st-size-offset) 32 (endianness little))
+              ;; Set the size field
+              (bytevector-u64-set! table (+ entry-offset st-size-offset) (if is-function 0 32) (endianness little))
               (loop (cdr symbols)
                     (+ index 1)
                     (+ str-offset (string-length name) null-terminator-size))))))))
-
-(define* (create-table symbol-addresses create-entry-func table-type #:optional (options '()))
-  (let ((stt-object (or (assoc-ref options 'stt-object) 1))
-        (stt-func (or (assoc-ref options 'stt-func) 2))
-        (stb-global (or (assoc-ref options 'stb-global) 1))
-        (shn-text (or (assoc-ref options 'shn-text) 1))
-        (shn-data (or (assoc-ref options 'shn-data) 2))
-        (symbol-entry-size (or (assoc-ref options 'symbol-entry-size) 24)))
-    (let* ((symbol-count (+ (length symbol-addresses) 1))
-           (entries (cons (create-entry-func 'main #x1000 (logior (ash stb-global 4) stt-func) 0 shn-text 0)
-                          (map (lambda (sym)
-                                 (create-entry-func (car sym) (cdr sym) (logior (ash stb-global 4) stt-object) 0 shn-data 32))
-                               symbol-addresses))))
-      (symbols->bytevector entries symbol-entry-size))))
-
-(define (create-symbol-entry name address info other shndx size)
-  (make-symbol-entry name address info other shndx size))
-
-(define (symbols->bytevector entries symbol-entry-size)
-  (let* ((table-size (* (length entries) symbol-entry-size))
-         (table (make-bytevector table-size 0)))
-    (let loop ((entries entries)
-               (index 0))
-      (if (null? entries)
-          table
-          (let* ((entry (car entries))
-                 (address (symbol-entry-address entry))
-                 (entry-offset (* index symbol-entry-size)))
-            (bytevector-u64-set! table entry-offset address (endianness little))
-            (loop (cdr entries)
-                  (+ index 1)))))))
 
 (define* (create-hash-section dynsym-table #:optional (options '()))
   (let ((hash-header-size (or (assoc-ref options 'hash-header-size) 8))
--- test-symbol-table.scm
+++ test-symbol-table.scm
@@ -4,12 +4,6 @@
 
 (define little (endianness little))
 
-(define (bytevector->list bv)
-  (let loop ((i 0) (lst '()))
-    (if (= i (bytevector-length bv))
-        (reverse lst)
-        (loop (+ i 1) (cons (bytevector-u8-ref bv i) lst)))))
-
 (test-begin "symbol-table")
 
 ;; Test make-symbol-table function
@@ -26,23 +20,55 @@
     (test-assert "non-existent symbol should return #f"
       (not (get-symbol table 'symbol2)))))
 
-;; Test create-symbol-table function
+;; New test group for label symbols
+(test-group "label-symbols"
+  (let ((table (make-symbol-table)))
+    (test-assert "Add label symbol"
+      (begin
+        (add-label-symbol! table 'main #x1000)
+        (get-symbol table 'main)))
+    
+    (test-eqv "Get label symbol address"
+      #x1000
+      (get-symbol table 'main))
+    
+    (test-assert "Label symbol should be in symbol table"
+      (symbol-in-table? table 'main))
+    
+    (test-assert "Label symbol should be marked as a function"
+      (symbol-is-function? table 'main))
+    
+    (test-assert "Data symbol should not be marked as a function"
+      (begin
+        (add-symbol! table 'data1 #x2000)
+        (not (symbol-is-function? table 'data1))))))
+
+;; Updated test for create-symbol-table function
 (test-group "create-symbol-table"
   (let* ((symbol-addresses '((symbol1 . #x1000) (symbol2 . #x2000)))
-         (result (create-symbol-table symbol-addresses))
-         (sym-table (car result))
-         (string-table (cdr result)))
-    (test-assert "symbol table bytevector should be of the correct size"
-      (= (bytevector-length sym-table) (* (+ (length symbol-addresses) 1) 24)))
-    (test-assert "string table should be created"
-      (bytevector? string-table))))
+         (label-positions '((main . #x3000) (func1 . #x3100)))
+         (sym-table (create-symbol-table symbol-addresses label-positions)))
+    (test-assert "data symbols should be in the table"
+      (and (symbol-in-table? sym-table 'symbol1)
+           (symbol-in-table? sym-table 'symbol2)))
+    (test-assert "label symbols should be in the table"
+      (and (symbol-in-table? sym-table 'main)
+           (symbol-in-table? sym-table 'func1)))
+    (test-assert "label symbols should be marked as functions"
+      (and (symbol-is-function? sym-table 'main)
+           (symbol-is-function? sym-table 'func1)))
+    (test-assert "data symbols should not be marked as functions"
+      (and (not (symbol-is-function? sym-table 'symbol1))
+           (not (symbol-is-function? sym-table 'symbol2))))))
 
 ;; Test create-dynamic-symbol-table function
 (test-group "create-dynamic-symbol-table"
   (let* ((symbol-addresses '((symbol1 . #x1000) (symbol2 . #x2000)))
-         (dynsym-table (create-dynamic-symbol-table symbol-addresses)))
+         (label-positions '((main . #x3000) (func1 . #x3100)))
+         (sym-table (create-symbol-table symbol-addresses label-positions))
+         (dynsym-table (create-dynamic-symbol-table sym-table)))
     (test-assert "dynamic symbol table bytevector should be of the correct size"
-      (= (bytevector-length dynsym-table) (* (+ (length symbol-addresses) 1) 24)))
+      (= (bytevector-length dynsym-table) (* (+ (length symbol-addresses) (length label-positions) 1) 24)))
     (test-equal "first entry name offset should be zero"
       0 (bytevector-u32-ref dynsym-table 0 little))
     (test-equal "first entry info should be zero"
@@ -53,37 +79,33 @@
   ;; Test with maximum address value
   (let* ((max-address #xFFFFFFFFFFFFFFFF)
          (symbol-addresses `((symbol1 . ,max-address)))
-         (dynsym-table (create-dynamic-symbol-table symbol-addresses)))
+         (label-positions '((main . #x1000)))
+         (sym-table (create-symbol-table symbol-addresses label-positions))
+         (dynsym-table (create-dynamic-symbol-table sym-table)))
     (test-equal "dynamic symbol table should handle maximum address value"
       max-address (bytevector-u64-ref dynsym-table 32 little)))
 
   ;; Test for endianness with a known pattern
   (let* ((test-address #x0102030405060708)
          (symbol-addresses `((symbol1 . ,test-address)))
-         (dynsym-table (create-dynamic-symbol-table symbol-addresses)))
+         (label-positions '((main . #x1000)))
+         (sym-table (create-symbol-table symbol-addresses label-positions))
+         (dynsym-table (create-dynamic-symbol-table sym-table)))
     (test-equal "dynamic symbol table should handle endianness correctly"
       test-address (bytevector-u64-ref dynsym-table 32 little))))
 
 ;; Test create-hash-section function
 (test-group "create-hash-section"
   (let* ((symbol-addresses '((symbol1 . #x1000) (symbol2 . #x2000)))
-         (dynsym-table (create-dynamic-symbol-table symbol-addresses))
+         (label-positions '((main . #x3000) (func1 . #x3100)))
+         (sym-table (create-symbol-table symbol-addresses label-positions))
+         (dynsym-table (create-dynamic-symbol-table sym-table))
          (hash-section (create-hash-section dynsym-table)))
     (test-assert "hash section bytevector should be of the correct size"
-      (= (bytevector-length hash-section) (+ 8 (* 4 1) (* 4 (+ (length symbol-addresses) 1)))))
+      (= (bytevector-length hash-section) (+ 8 (* 4 1) (* 4 (+ (length symbol-addresses) (length label-positions) 1)))))
     (test-equal "number of buckets should be 1"
       1 (bytevector-u32-ref hash-section 0 little))
-    (test-equal "number of chains should match the number of symbols"
-      (+ (length symbol-addresses) 1) (bytevector-u32-ref hash-section 4 little))))
-
-;; Test alignment and padding in Symbol Table
-(test-group "alignment-and-padding-in-symbol-table"
-  (let* ((symbol-addresses '((symbol1 . #x1000) (symbol2 . #x2000)))
-         (result (create-symbol-table symbol-addresses))
-         (sym-table (car result)))
-    (test-equal "symbol table should have correct alignment for second entry"
-      #x1000 (bytevector-u64-ref sym-table 32 little))
-    (test-equal "symbol table should have correct alignment for third entry"
-      #x2000 (bytevector-u64-ref sym-table 56 little))))
+    (test-equal "number of chains should match the total number of symbols"
+      (+ (length symbol-addresses) (length label-positions) 1) (bytevector-u32-ref hash-section 4 little))))
 
 (test-end "symbol-table")
