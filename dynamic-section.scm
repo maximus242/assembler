ff3bb35eb7b229ee10ca859e3ab8df2131cc5276
Philip Solobay <maximus242@gmail.com>
2024-08-23 10:51:04 -0500

    fix dynamic section

--- dynamic-section.scm
+++ dynamic-section.scm
@@ -1,5 +1,6 @@
 (define-module (dynamic-section)
   #:use-module (rnrs bytevectors)
+  #:use-module (rnrs io simple)
   #:export (create-dynamic-section))
 
 ;; Constants for dynamic section entry tags
@@ -32,7 +33,8 @@
 (define (set-dynamic-entry! section index tag value)
   (let ((offset (* index ENTRY_SIZE)))
     (bytevector-u64-set! section offset tag (endianness little))
-    (bytevector-u64-set! section (+ offset VALUE_OFFSET) value (endianness little))))
+    (bytevector-u64-set! section (+ offset VALUE_OFFSET) value (endianness little))
+    (format #t "Set entry: index=~a, tag=~a, value=~a~%" index tag value)))
 
 (define (create-dynamic-section
           dynstr-offset dynsym-offset strtab-size dynsym-size 
@@ -41,6 +43,11 @@
           plt-offset plt-size jmprel-offset jmprel-size)
   (let* ((num-entries (if (> gnu-version-r-size 0) 18 16))
          (section (make-bytevector (* num-entries ENTRY_SIZE) 0)))
+    (format #t "Creating dynamic section with num-entries=~a~%" num-entries)
+    
+    ;; Log initial parameters
+    (format #t "Parameters: jmprel-offset=~a, jmprel-size=~a~%" jmprel-offset jmprel-size)
+    
     (set-dynamic-entry! section 0 DT_HASH hash-offset)
     (set-dynamic-entry! section 1 DT_STRTAB dynstr-offset)
     (set-dynamic-entry! section 2 DT_SYMTAB dynsym-offset)
@@ -50,11 +57,14 @@
     (set-dynamic-entry! section 6 DT_RELASZ rela-size)
     (set-dynamic-entry! section 7 DT_RELAENT RELAENT_SIZE)
     (set-dynamic-entry! section 8 DT_PLTGOT got-offset)
+    
     ;; Ensure both DT_JMPREL and DT_PLTRELSZ are set if jmprel-offset and jmprel-size are provided
     (when (> jmprel-size 0)
+      (format #t "Setting DT_PLTRELSZ and DT_JMPREL entries~%")
       (set-dynamic-entry! section 9 DT_PLTRELSZ jmprel-size)
       (set-dynamic-entry! section 10 DT_PLTREL DT_RELA)
       (set-dynamic-entry! section 11 DT_JMPREL jmprel-offset))
+    
     ;; Handle optional init, fini, and GNU version entries
     (set-dynamic-entry! section 12 DT_INIT 0)  ; Set to 0 if not used
     (set-dynamic-entry! section 13 DT_FINI 0)  ; Set to 0 if not used
@@ -65,4 +75,7 @@
           (set-dynamic-entry! section 16 DT_VERNEEDNUM 1)
           (set-dynamic-entry! section 17 DT_NULL 0))
         (set-dynamic-entry! section 15 DT_NULL 0))
+    
+    ;; Final logging
+    (format #t "Dynamic section creation complete.~%")
     section))
